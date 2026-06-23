255ab0cd0eea7e1a5d8e59b55650109f1dc0d611
윤수영 <market_designer@naver.com>
2026-06-23 17:09:46 +0900

    저장 문제 해결 및 마이페이지 결과 삭제 기능, 카드 이미지 크기 조정

--- kk_page/kk-card-back/cardlist/urls.py
+++ kk_page/kk-card-back/cardlist/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.card_list),
     path("recommend/", views.recommend_cards),
     path("results/", views.recommend_results, name="recommend-results"),
+    path("results/<int:result_pk>/", views.recommend_result_detail, name="recommend_result_detail"),
     path("<int:card_pk>/", views.card_detail),
 ]
